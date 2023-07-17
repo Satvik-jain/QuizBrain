82f33e6753f5357e28b8f5e04b593ab7b34e7e28
satvik <satvik.jain.kht@gmail.com>
2023-07-17 19:47:24 +0530

    Quiz Brain

---
+++ main.py
@@ -0,0 +1,12 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank=[]
+for question_set in question_data:
+    question_bank.append(Question(question_set["text"], question_set["answer"]))
+
+quiz = QuizBrain(question_bank)
+while quiz.question_left():
+    quiz.next_question()
+
